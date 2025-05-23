49f00c1af5eff99a1e305f7a5651da77742fab4c
Yan Broetto <ybroetto@hotmail.com>
2025-05-23 11:55:52 -0300

    Generated jards for 1.21.4 and 1.21.5

---
+++ remappedSrc/io/blodhgarm/knock_on_doors/EasyJsonConfig.java
@@ -0,0 +1,126 @@
+package io.blodhgarm.knock_on_doors;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.mojang.logging.LogUtils;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class EasyJsonConfig<T> implements SimpleSynchronousResourceReloadListener {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private final Gson gson;
+    private final Identifier configName;
+
+    private final Supplier<Path> configPathSup;
+
+    private final Function<JsonObject, T> reader;
+    private final Supplier<JsonObject> factory;
+
+    @Nullable
+    private T instance = null;
+
+    public EasyJsonConfig(Identifier configName, Supplier<Path> configPathSup, Function<JsonObject, T> reader, Supplier<JsonObject> constructor) {
+        this(gsonBuilder -> {}, configName, configPathSup, reader, constructor);
+    }
+
+    public EasyJsonConfig(Consumer<GsonBuilder> builderConsumer, Identifier configName, Supplier<Path> configPathSup, Function<JsonObject, T> reader, Supplier<JsonObject> factory) {
+        var builder = new GsonBuilder().setPrettyPrinting();
+
+        builderConsumer.accept(builder);
+
+        this.gson = builder.create();
+        this.configName = configName;
+
+        this.configPathSup = configPathSup;
+
+        this.reader = reader;
+        this.factory = factory;
+
+        init();
+    }
+
+    @Nullable
+    public T instance() {
+        return this.instance;
+    }
+
+    public void init() {
+        File configFolder = configPathSup.get().resolve(configName.getNamespace()).toFile();
+
+        if (!configFolder.exists()) {
+            if (!configFolder.mkdir()) {
+                LOGGER.warn("[EasyJsonConfig({})] Could not create configuration directory: {}", configName, configFolder.getAbsolutePath());
+            }
+        }
+
+        JsonObject configObject = null;
+
+        File configFile = new File(configFolder, configName.getPath() + ".json");
+
+        try {
+            if (!configFile.exists()) {
+                LOGGER.info("[EasyJsonConfig({})]: Unable to find needed config file, will attempt to create such.", configName);
+
+                configObject = factory.get();
+
+                Files.createFile(configFile.toPath());
+
+                try (FileWriter writer = new FileWriter(configFile)) {
+                    writer.write(gson.toJson(configObject));
+                }
+            } else {
+                configObject = gson.fromJson(Files.readString(configFile.toPath()), JsonObject.class);
+
+                if (configObject == null) throw new IOException("The config file was not found!");
+            }
+        } catch (IOException exception) {
+            LOGGER.error("[EasyJsonConfig({})]: Unable to create the needed config file, using default values!", configName, exception);
+        } catch (JsonSyntaxException exception) {
+            LOGGER.error("[EasyJsonConfig({})]: Unable to read the needed config file, using default values!", configName, exception);
+        }
+
+        try {
+            if(configObject == null) configObject = this.factory.get();
+
+            this.instance = reader.apply(configObject);
+
+            LOGGER.info("[EasyJsonConfig({})]: Loaded Config File!", configName);
+        } catch (Exception e) {
+            LOGGER.error("[EasyJsonConfig({})]: Unable to deserialize the needed config, using default values!", configName, e);
+
+            configObject = factory.get();
+
+            try (FileWriter writer = new FileWriter(configFile)) {
+                writer.write(gson.toJson(configObject));
+            } catch (IOException exception) {
+                LOGGER.error("[EasyJsonConfig({})]: Unable to fix the needed config file, using default values!", configName, exception);
+            }
+        }
+    }
+
+    @Override
+    public Identifier getFabricId() {
+        return Identifier.of("easy_json_config", configName.toString().replace(":", "/"));
+    }
+
+    @Override
+    public void reload(ResourceManager manager) {
+        init();
+    }
+}
---
+++ remappedSrc/io/blodhgarm/knock_on_doors/KnockOnDoors.java
@@ -0,0 +1,69 @@
+package io.blodhgarm.knock_on_doors;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.BlockState;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
+
+public class KnockOnDoors implements ModInitializer {
+
+    public static final String MODID = "knock_on_doors";
+
+    public static final EasyJsonConfig<Config> CONFIG_HOLDER = new EasyJsonConfig<>(KnockOnDoors.id("main"), FabricLoader.getInstance()::getConfigDir,
+            (object) -> {
+                var volumeElement = object.get("volume");
+
+                return new Config(volumeElement.getAsFloat());
+            },
+            () -> {
+                var object = new JsonObject();
+
+                object.addProperty("volume", 0.8f);
+
+                return object;
+            }
+    );
+
+    public void onInitialize() {
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if(!player.isSpectator() && !player.isCreative() && player.getMainHandStack().isEmpty()) {
+                var blockState = world.getBlockState(pos);
+
+                float pitch;
+                boolean isWooden;
+
+                if (blockState.isIn(BlockTags.DOORS)) {
+                    isWooden = blockState.isIn(BlockTags.WOODEN_DOORS);
+                    pitch = 0.6f;
+                } else if (blockState.isIn(BlockTags.TRAPDOORS)) {
+                    isWooden = blockState.isIn(BlockTags.WOODEN_TRAPDOORS);
+                    pitch = 4.0f;
+                } else {
+                    return ActionResult.PASS;
+                }
+
+                var soundEvent = isWooden
+                        ? SoundEvents.ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR
+                        : SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR;
+
+                world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, CONFIG_HOLDER.instance().volume(), pitch);
+            }
+
+            return ActionResult.PASS;
+        });
+    }
+
+    public record Config(float volume) {}
+
+    public static Identifier id(String path) {
+        return Identifier.of(MODID, path);
+    }
+}
