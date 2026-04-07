ae48edb35deb87a863c2afa1300a25b07d42c424
Jaime Nathaniel Hernández Rivera <jnatheriver@outlook.com>
2026-04-06 22:50:25 -0600

    Update to 26.1, migrating from yarn mappings to mojang mappings

--- src/main/java/com/crimulus/knock_on_doors/KnockOnDoors.java
+++ src/main/java/com/crimulus/knock_on_doors/KnockOnDoors.java
@@ -4,11 +4,11 @@ import com.google.gson.JsonObject;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.Identifier;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionResult;
 
 public class KnockOnDoors implements ModInitializer {
 
@@ -31,36 +31,36 @@ public class KnockOnDoors implements ModInitializer {
 
     public void onInitialize() {
         AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if(!player.isSpectator() && !player.isCreative() && player.getMainHandStack().isEmpty()) {
+            if(!player.isSpectator() && !player.isCreative() && player.getMainHandItem().isEmpty()) {
                 var blockState = world.getBlockState(pos);
 
                 float pitch;
                 boolean isWooden;
 
-                if (blockState.isIn(BlockTags.DOORS)) {
-                    isWooden = blockState.isIn(BlockTags.WOODEN_DOORS);
+                if (blockState.is(BlockTags.DOORS)) {
+                    isWooden = blockState.is(BlockTags.WOODEN_DOORS);
                     pitch = 0.6f;
-                } else if (blockState.isIn(BlockTags.TRAPDOORS)) {
-                    isWooden = blockState.isIn(BlockTags.WOODEN_TRAPDOORS);
+                } else if (blockState.is(BlockTags.TRAPDOORS)) {
+                    isWooden = blockState.is(BlockTags.WOODEN_TRAPDOORS);
                     pitch = 4.0f;
                 } else {
-                    return ActionResult.PASS;
+                    return InteractionResult.PASS;
                 }
 
                 var soundEvent = isWooden
-                        ? SoundEvents.ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR
-                        : SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR;
+                        ? SoundEvents.ZOMBIE_ATTACK_WOODEN_DOOR
+                        : SoundEvents.ZOMBIE_ATTACK_IRON_DOOR;
 
-                world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, CONFIG_HOLDER.instance().volume(), pitch);
+                world.playSound(null, pos, soundEvent, SoundSource.BLOCKS, CONFIG_HOLDER.instance().volume(), pitch);
             }
 
-            return ActionResult.PASS;
+            return InteractionResult.PASS;
         });
     }
 
     public record Config(float volume) {}
 
     public static Identifier id(String path) {
-        return Identifier.of(MODID, path);
+        return Identifier.fromNamespaceAndPath(MODID, path);
     }
 }
